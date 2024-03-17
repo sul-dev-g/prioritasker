faa443efce1ef9077fcbed2f7d3b11c732bd72d2
Sul Kamara <sulaimankamara110@gmail.com>
2024-03-17 15:43:38 +0000

    open menu when click on hamburger icon

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Accessing elements
-const navLinks = document.querySelector("#nav-links");
 const toggleMenuButton = document.querySelector("#toggle-menu-button");
 const toggleMenuIcon = document.querySelector("#toggle-menu-icon");
 const line = document.querySelector(".line");
@@ -12,6 +11,7 @@ function openMenu() {
   const hamburgerTopBar = document.querySelector(".top-bar");
   const hamburgerMiddleBar = document.querySelector(".middle-bar");
   const hamburgerBottomBar = document.querySelector(".bottom-bar");
+  const navLinks = document.querySelector("#nav-links");
 
   // Turn hamburger into an X
   hamburgerTopBar.classList.toggle("rotate-45");
@@ -19,17 +19,7 @@ function openMenu() {
   hamburgerBottomBar.classList.toggle("rotate-315");
   hamburgerBottomBar.classList.toggle("center-vertically");
   hamburgerTopBar.classList.toggle("center-vertically");
+
+  // Show nav links
+  navLinks.classList.toggle("hide");
 }
-
-// // const lineBefore = window.getComputedStyle(line, "::before");
-// const lineAfter = window.getComputedStyle(line, ":after");
-// const containsBarsIcon = toggleMenuIcon.classList.contains("fa-bars");
-// const removeBarsIcon = toggleMenuIcon.classList.remove("fa-bars");
-// const replaceWithCloseIcon = toggleMenuIcon.classList.add("fa-xmark");
-// const removeCloseIcon = toggleMenuIcon.classList.remove("fa-xmark");
-// const replaceWithBarsIcon = toggleMenuIcon.classList.add("fa-bars");
-
-// containsBarsIcon
-//   ? removeBarsIcon && replaceWithCloseIcon
-//   : removeCloseIcon && replaceWithBarsIcon;
-// target issue maybe you need to use target on the toggleMenuButton or toggleMenuIcon because maybe the browser can't distinguish between what is being clicked or maybe z index will work
