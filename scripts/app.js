322e5fa7f65339f11be1855c9e124cbfbe424645
Sul Kamara <sulaimankamara110@gmail.com>
2024-04-16 11:41:13 +0100

    change directory structure

--- scripts/app.js
+++ scripts/app.js
@@ -78,6 +78,10 @@ const submitTask = function (e) {
 
   taskRecordObject.contents = `${task}`;
   taskRecordObject.isComplete = false;
+
+  // Go to next screen
+  window.location.href =
+    "https://sul-devs.github.io/prioritasker/app/screens/answer-quiz.html";
 };
 
 submitTaskForm.addEventListener("submit", submitTask);
